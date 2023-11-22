de9a74c5b668fa96cf9eec8869d70eaa6f615d5f
LeilaelRico <A01707023@tec.mx>
2023-11-22 11:09:32 +0100

    Prior to code cleanup.

--- P3/clreCrawler.py
+++ P3/clreCrawler.py
@@ -10,37 +10,42 @@ Práctica 3: Crawler Avanzado
 import requests
 from bs4 import BeautifulSoup
 import random
+from time import sleep
+import re
 
 # Creation of txt where site's title and link will be saved
 archivo = open("sitesVisited.txt", "w", encoding='utf-8')
 
 
 # Function that calls the crawler for future recursion.
+# Timeout instruction tells function to wait 5 seconds before doing a new get.
 def creWikiCrawler(url):
 	response = requests.get(
-		url=url,
+		url=url, timeout=3,
 	)
 
 	dataPull = BeautifulSoup(response.content, 'html.parser')
 
 	wikiTitle = dataPull.find(id="firstHeading")
-	print(url)
+	# print(url)
 	archivo.write(wikiTitle.text + ":    " + url + "\n")
 
-	findAllLinks = dataPull.find(id="bodyContent").find_all("a")
+	findAllLinks = dataPull.find(id="bodyContent").find_all(
+		"a", href=re.compile("^(/wiki/)"))
+	# print(findAllLinks)
 	random.shuffle(findAllLinks)
+	# print(findAllLinks)
 	linkToScrape = 0
 
 	for link in findAllLinks:
-		# Condition will check and choose link that is part of a wiki.
-		if link['href'].find("/wiki/") == -1:
+
+		# Condition will check and choose link that is part of a wiki from Wikipedia Spain.
+		if link['href'].find("es.wikipedia.org"):
 			continue
 
-		# Link is used to scrape
-		linkToScrape = link
-		break
+	print("https://es.wikipedia.org" + link['href'])
 
-	creWikiCrawler("https://es.wikipedia.org" + linkToScrape['href'])
+	creWikiCrawler("https://es.wikipedia.org" + link['href'])
 
 
 creWikiCrawler("https://es.wikipedia.org/wiki/Wikipedia:Portada")
